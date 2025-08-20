cfd7cb39e02c12869ed14bd5b35856f0d0015f7c
manish <manishgupta23956@gmail.com>
2025-08-20 16:18:41 +0530

    Solved sum of N numbers

--- Q6.c
+++ Q6.c
@@ -1,1 +1,14 @@
 //Sum of N natural numbers using loop
+#include<stdio.h>
+int main()
+{
+    int n,sum=0;
+    printf("Enter the Number\n");
+    scanf("%d",&n);
+    for(int i=0;i<=n;i++)
+    {
+        sum+=i;
+    }
+    printf("%d",sum);
+    return 0;
+}
