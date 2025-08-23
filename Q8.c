af6f14a410b66cff9b79493c6d21fdedaca6fd3b
manish <manishgupta23956@gmail.com>
2025-08-23 18:50:48 +0530

    new file

--- Q8.c
+++ Q8.c
@@ -1,1 +1,15 @@
-//Print Fibonacci series upto n using loop
+// Print Fibonacci series upto n using loop
+#include <stdio.h>
+int main()
+{
+    int n, i, a = 0, b = 1, c;
+    printf("Enter a number : \n");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        printf("%d",a);
+        c=a+b;
+        a=b;
+        b=c;
+    }
+}
